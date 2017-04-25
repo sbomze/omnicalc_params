4e1647e0293448a29af8561213eb66975132bea9
Steve Bomze <sbomze@chicagobooth.edu>
2017-04-25 14:50:35 -0500

    v3

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,10 @@ Rails.application.routes.draw do
   get("/flexible/square_root/:num", {:controller => "calculations", :action => "flex_square_root_8"})
   get("/flexible/payment/:rate/:months/:principal", {:controller => "calculations", :action => "flex_payment"})
   get("/flexible/random/:rand1/:rand2", {:controller => "calculations", :action => "flex_random"})
-
-
   get("/square/new", :controller => "calculations", :action =>"square_form")
+
+
+  
   get("/square/results", :controller => "calculations", :action =>"square")
 
 end
