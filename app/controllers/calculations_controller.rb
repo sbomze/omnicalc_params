4bb1b876d2581c755718ba4aeb41d2cd937ee2f3
Steve Bomze <sbomze@chicagobooth.edu>
2017-04-25 15:01:51 -0500

    v3

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -1,15 +1,5 @@
 class CalculationsController <ApplicationController
 
-  def square
-    @user_number = params[:user_number].to_f
-    @square = @user_number**2
-    render("calculations/square.html.erb")
-  end
-
-  def square_form
-    render("calculations/square_form.html.erb")
-  end
-
   def flex_square_5
     @user_number = params["num"].to_f
     @square = @user_number**2
@@ -37,4 +27,14 @@ class CalculationsController <ApplicationController
     render("calculations/flex_random.html.erb")
   end
 
+  def square
+    @user_number = params[:user_number].to_f
+    @square = @user_number**2
+    render("calculations/square.html.erb")
+  end
+
+  def square_form
+    render("calculations/square_form.html.erb")
+  end
+  
 end
